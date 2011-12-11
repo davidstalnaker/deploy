3635859ba16cae88be458ed2f0f6cdf826b96763
David Stalnaker <david.stalnaker@gmail.com>
2011-12-11 05:35:29 +0000

    reorganized a lot of things and handle errors better

--- deploy.py
+++ deploy.py
@@ -5,6 +5,7 @@ from config import Config
 import json
 import re
 import os
+import pwd
 
 app = Flask(__name__)
 config = Config('deploy.conf')
@@ -17,35 +18,64 @@ def reload():
 	
 	for repo in config.repos:
 		if urls_are_equal(url, repo.remote):
-			pull_and_reload(repo.location, repo.reload_command)
+			success = pull(repo)
+			if success and 'reload_command' in repo:
+				call(repo.reload_command, shell=True)
 	return ""
 	
-def pull_and_reload(repo_location, reload_command):
-	stat = os.stat(repo_location)
-	uid = stat.st_uid
-	gid = stat.st_gid
+def pull(repo):
+	try:
+		if 'git_user' in repo:
+			uid, gid = ids_for_user(repo.git_user)
+		else:
+			uid, gid = ids_for_file(repo.location)
+		become_user(uid, gid)
+		
+		r = Repo(repo.location)
+		info = r.remotes.origin.pull()[0]
+				
+		if info.flags & info.REJECTED:
+			print('Error: merge failed')
+			return False
+		elif info.flags & info.HEAD_UPTODATE:
+			print('Head already up to date')
+			return True
+		else:
+			return True
+		
+	except OSError:
+		print('Insufficient permissions to change uid/gid')
+		return False
+	except ValueError:
+		print('Insufficient permissions to pull repository')
+		return False
+	except AssertionError as e:
+		if 'overwritten by merge' in e.message:
+			print('Error: merge conflict')
+		else:
+			print('Error: %s' % e.message)
+		return False
+	finally:
+		become_root();
+		
+
+	
+		
+def ids_for_user(username):
+	pw = pwd.getpwnam('max')
+	return (pw.pw_uid, pw.pw_gid)
+		
+def ids_for_file(filename):
+	st = os.stat(filename)
+	return (st.st_uid, st.st_gid)	
+	
+def become_user(uid, gid):
 	os.setegid(gid)
 	os.seteuid(uid)
 	
-	r = Repo(repo_location)
-	try:
-		info = r.remotes.origin.pull()[0]
-		os.seteuid(0)
-		os.setegid(0)
-		if info.flags & info.REJECTED:
-			print('error: merge failed')
-		elif info.flags & info.HEAD_UPTODATE:
-			print('head already up to date')
-			call(reload_command, shell=True)
-		else:
-			call(reload_command, shell=True)
-	except Exception as e:
-		raise(e)
-		print('error: merge failed')
-		
-	
-	
-	
+def become_root():
+	os.seteuid(0)
+	os.setegid(0)
 	
 def urls_are_equal(first, second):
 	first = re.split('://', first)[-1]
